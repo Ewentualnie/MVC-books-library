44401e36ef0a694aac7641d8c91a423aeb823b5a
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-02-15 13:44:14 +0200

    import path and router.ts, set views

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path'
+import router from "./routers/router";
 
 const app = express();
-
-app.use(express.static('source'))
-app.use(express.json())
-app.use(morgan('common'));
-
+const source = path.resolve(__dirname, '../source')
 const port: number = 3000;
 
+app.set('view engine', 'ejs')
+app.set('views', source)
+
+app.use(express.static(source))
+app.use(express.json())
+app.use(morgan('common'));
+app.use(router)
 
 app.listen(port, () => console.log("app listen on port " + port))
