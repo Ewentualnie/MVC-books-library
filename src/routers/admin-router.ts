bda33a2c8f4f2e02e2cb387787a958c8bd9c51be
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-02-23 19:17:38 +0200

    test method to see result admin page

--- src/routers/admin-router.ts
+++ src/routers/admin-router.ts
@@ -3,7 +3,18 @@ import express, {Request, Response} from "express";
 const adminRouter = express.Router();
 
 adminRouter.get('/', (req: Request, res: Response) => {
-    res.render("admin", {})
+    res.render("admin", {
+        books: [
+            {id: 37, name: "37.jpg", authors: ["author", "author"], year: 2022, clickCount: 5},
+            {id: 25, name: "37.jpg", authors: ["author", "author"], year: 2022, clickCount: 5},
+            {id: 14, name: "37.jpg", authors: ["author", "author"], year: 2022, clickCount: 5},
+            {id: 38, name: "37.jpg", authors: ["author", "author"], year: 2022, clickCount: 5},
+            {id: 37, name: "37.jpg", authors: ["author", "author"], year: 2022, clickCount: 5},
+            {id: 89, name: "37.jpg", authors: ["author", "author"], year: 2022, clickCount: 5},
+            {id: 88, name: "37.jpg", authors: ["author", "author"], year: 2022, clickCount: 5},
+
+        ]
+    })
 }).post('/api/v1', (req: Request, res: Response) => {
 
 }).delete('/api/v1', (req: Request, res: Response) => {
