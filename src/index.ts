a0314ab914eea87b80586900f2906d693835fb8b
Serhii Zaika <ewentualnie.sz@gmail.com>
2023-03-09 14:57:29 +0200

    add import body-parser

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import path from 'path'
 import booksRouter from "./routers/books-router";
 import adminRouter from "./routers/admin-router"
+import bodyParser from 'body-parser'
 
 const app = express();
 const source = path.resolve(__dirname, '../source')
@@ -11,6 +12,7 @@ const port: number = 3000;
 app.set('view engine', 'ejs')
 app.set('views', source)
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(source))
 app.use(express.json())
 app.use(morgan('common'));
