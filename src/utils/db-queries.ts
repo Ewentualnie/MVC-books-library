fcbd8f6357c45cb865d34090e19cd48769456984
serhii <ewentualnie.sz@gmail.com>
2023-03-22 14:27:40 +0200

    :sparkles: replace variables with functions

--- src/utils/db-queries.ts
+++ src/utils/db-queries.ts
@@ -1,2 +1,18 @@
-export const queryAll = 'SELECT * FROM books;'
-export const queryBook = 'SELECT * FROM books WHERE ID ='
+const tableName: string = process.env.tableNmame || 'books'
+
+export function queryAll() {
+    return `SELECT *
+            FROM ${tableName};`
+}
+
+export function queryBook(id: number) {
+    return `SELECT *
+            FROM ${tableName}
+            WHERE ID = ${id}`
+}
+
+export function queryWithLimit(limit: number, position: number) {
+    return `SELECT *
+            FROM ${tableName}
+            LIMIT ${position}, ${limit}`
+}
