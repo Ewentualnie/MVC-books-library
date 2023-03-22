5aac6ef5ef998023770a0036f889e4a59a3ee756
serhii <ewentualnie.sz@gmail.com>
2023-03-22 14:27:05 +0200

    :sparkles: replace arrow-function with usual functions, improve imports

--- src/controllers/admin-controller.ts
+++ src/controllers/admin-controller.ts
@@ -1,19 +1,22 @@
 import {Request, Response} from "express";
-import database from "../utils/db-connection";
-import {queryAll} from "../utils/db-queries";
 import fs from "fs";
+import {findAll} from "../services/mysql-service";
 
-export const getAll = async (req: Request, res: Response) =>
+export async function getAll(req: Request, res: Response) {
     res.render('admin', {
-        books: (await database.query(queryAll))[0]
+        books: await findAll()
     })
-export const addBook = (req: Request, res: Response) => {
+}
+
+export function addBook(req: Request, res: Response) {
     console.log(req.body)
     res.redirect('..')
 }
-export const deleteBook = (req: Request, res: Response) => {
+
+export function deleteBook(req: Request, res: Response) {
 
 }
-export const getFront = (req: Request, res: Response) => {
+
+export function getFront(req: Request, res: Response) {
     fs.readFile('./dist/front/admin.js', 'utf-8', (err, data) => res.send(data))
 }
