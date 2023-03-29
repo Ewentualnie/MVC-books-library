647dfa53e37995e2c9dea1c4323d2274093a6b69
serhii <ewentualnie.sz@gmail.com>
2023-03-30 00:02:11 +0300

    :heavy_plus_sign: add pagination on admin page, implement addBook function (not yet)

--- src/controllers/admin-controller.ts
+++ src/controllers/admin-controller.ts
@@ -1,15 +1,44 @@
 import {Request, Response} from "express";
 import fs from "fs";
-import {findAll} from "../services/mysql-service";
+import {findAll, findByLimitAndCounter} from "../services/mysql-service";
+import {Book} from "../models/types";
+import {IncomingForm} from "formidable";
 
 export async function getAll(req: Request, res: Response) {
+    const middlePage: number = 2;
+    const limit: number = +(process.env.adminLimit || 5);
+    const page: number = +(req.query.page || 0);
+    const length: number = (await findAll()).length;
+    const pageCount: number = Math.ceil(length / limit);
+    const buttonCount: number = 5;
+    console.log(req.query)
+    const books: [Book] = await findByLimitAndCounter(limit, page * limit)
+
     res.render('admin', {
-        books: await findAll()
+        books,
+        pageCount,
+        prev: page >= middlePage,
+        next: page <= pageCount - middlePage - 1,
+        page,
+        buttonCount
     })
 }
 
-export function addBook(req: Request, res: Response) {
-    console.log(req.body)
+export async function addBook(req: Request, res: Response) {
+    const form = new IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        const book: Book = {
+            name: req.body.name,
+            year: req.body.year,
+            author: req.body.author1,
+            preview: req.body.preview,
+            description: req.body.description,
+            title: req.body.author2
+        };
+        console.log(book)
+        console.log(files.preview)
+    });
+    // console.log(await save(book))
     res.redirect('..')
 }
 
