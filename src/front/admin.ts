5bb15c2bd6916db9fb04dc7f4075f873d73e1632
serhii <ewentualnie.sz@gmail.com>
2023-04-11 21:44:38 +0300

    :white_check_mark: add deleteBook function

--- src/front/admin.ts
+++ src/front/admin.ts
@@ -1,7 +1,15 @@
-import {FormDataBook} from "../models/types";
-
-function validateBook(dataBook: FormDataBook, e: Event) {
+function validateBook(dataBook: {
+                          name: { value: string; };
+                          year: { value: string | number; };
+                      },
+                      e: Event) {
     if (!dataBook.name.value || isNaN(+dataBook.year.value) || +dataBook.year.value == 0) {
         e.preventDefault();
     }
+}
+
+function deleteBook(id: number) {
+    fetch('./api/v1/' + id, {
+        method: "DELETE"
+    })
 }
