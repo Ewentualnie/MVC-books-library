d6df40b82c5659a16644e8e37b08eb65d6e7cb5c
serhii <ewentualnie.sz@gmail.com>
2023-04-23 22:56:24 +0300

    little changes, reload page after book deleting

--- src/front/admin.ts
+++ src/front/admin.ts
@@ -1,15 +1,19 @@
-function validateBook(dataBook: {
-                          name: { value: string; };
-                          year: { value: string | number; };
-                      },
-                      e: Event) {
+function validateBook(
+    dataBook: {
+        name: { value: string; };
+        year: { value: string | number; };
+    }, e: Event): void {
     if (!dataBook.name.value || isNaN(+dataBook.year.value) || +dataBook.year.value == 0) {
         e.preventDefault();
     }
 }
 
-function deleteBook(id: number) {
+function deleteBook(id: number): void {
     fetch('./api/v1/' + id, {
         method: "DELETE"
+    }).then((res: Response) => {
+        if (res.ok) {
+            window.location.reload();
+        }
     })
 }
