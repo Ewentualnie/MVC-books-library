96fdcce18177744a95566413fb1ef4cf55570c25
serhii <ewentualnie.sz@gmail.com>
2023-04-23 23:00:24 +0300

    little changes, add deleting book possibility

--- src/services/mysql-service.ts
+++ src/services/mysql-service.ts
@@ -3,27 +3,31 @@ import query from "../utils/db-queries";
 import {Book} from "../models/types";
 
 export async function findLength(): Promise<number> {
-    return (await database.query(query.getLength))[0][0]['COUNT(*)']
+    return (await database.query(query.getLength))[0][0]['COUNT(*)'];
 }
 
 export async function findAll(): Promise<[Book]> {
-    return (await database.query(query.getAll))[0]
+    return (await database.query(query.getAll))[0];
 }
 
 export async function findByLimitAndCounter(limit: number, counter: number): Promise<[Book]> {
-    return (await database.query(query.getWithLimit, [counter, limit]))[0]
+    return (await database.query(query.getWithLimit, [counter, limit]))[0];
 }
 
 export async function findById(id: number): Promise<Book> {
-    return (await database.query(query.getBookById, [id]))[0][0]
+    return (await database.query(query.getBookById, [id]))[0][0];
 }
 
-export async function save(book: Book) {
-    const storedBook = (await database.query(query.getBookByName, [book.name]))[0][0]
-    console.log(storedBook)
-    if (!storedBook) {
-        return await database.query(
+export async function saveBook(book: Book): Promise<number | undefined> {
+    const storedBook = (await database.query(query.getBookByName, [book.name]))[0]
+    return storedBook.lenght ?
+        undefined :
+        await database.query(
             query.addBook,
-            [book.name, book.year, book.author, book.description, book.preview, book.title])
-    }
+            [book.name, book.year, book.author, book.description, book.preview, book.title]
+        );
+}
+
+export async function deleteById(id: number): Promise<number> {
+    return (await database.query(query.deleteBook, [id]))[0].changedRows;
 }
