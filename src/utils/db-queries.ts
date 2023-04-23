f69999e0f0a05f0a0bebef65d5d4f39a28436743
serhii <ewentualnie.sz@gmail.com>
2023-04-23 22:58:10 +0300

    change sql queries, add deleteBook query

--- src/utils/db-queries.ts
+++ src/utils/db-queries.ts
@@ -1,19 +1,25 @@
 enum query {
     getLength = `SELECT COUNT(*)
-                 FROM books`,
+                 FROM books
+                 WHERE isDeleted = 0;`,
     getAll = `SELECT *
-              FROM books;`,
+              FROM books
+              WHERE isDeleted = 0;`,
     getBookById = `SELECT *
                    FROM books
-                   WHERE ID = ?`,
+                   WHERE ID = ? & isDeleted = 0`,
     getBookByName = `SELECT *
-                   FROM books
-                   WHERE name = ?`,
+                     FROM books
+                     WHERE name = ? & isDeleted = 0`,
     getWithLimit = `SELECT *
                     FROM books
+                    WHERE isDeleted = 0
                     LIMIT ?, ?;`,
-    addBook = `INSERT INTO books (name, year, author, description, preview, title)
-               VALUES (?, ?, ?, ?, ?, ?);`
+    addBook = `INSERT INTO books (name, year, author, description, preview, title, isDeleted)
+               VALUES (?, ?, ?, ?, ?, ?, 0);`,
+    deleteBook = `UPDATE books
+                  SET isDeleted = 1
+                  WHERE id = ?;`
 }
 
 export default query;
