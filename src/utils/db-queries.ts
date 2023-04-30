51f9e5f0832dc435cedfe1a87ce253c8bb28f6bf
serhii <ewentualnie.sz@gmail.com>
2023-04-30 12:18:09 +0300

    :construction: some fixes

--- src/utils/db-queries.ts
+++ src/utils/db-queries.ts
@@ -7,16 +7,16 @@ enum query {
               WHERE isDeleted = 0;`,
     getBookById = `SELECT *
                    FROM books
-                   WHERE ID = ?`,
+                   WHERE ID = ? AND isDeleted = 0`,
     getBookByName = `SELECT *
                      FROM books
-                     WHERE name = ? & isDeleted = 0`,
+                     WHERE name = ? AND isDeleted = 0`,
     getWithLimit = `SELECT *
                     FROM books
                     WHERE isDeleted = 0
                     LIMIT ?, ?;`,
-    addBook = `INSERT INTO books (name, year, author, description, preview, title, isDeleted)
-               VALUES (?, ?, ?, ?, ?, ?, 0);`,
+    addBook = `INSERT INTO books (name, year, author, description, preview, title, pages, isDeleted)
+               VALUES (?, ?, ?, ?, ?, ?, ?, 0);`,
     deleteBook = `UPDATE books
                   SET isDeleted = 1
                   WHERE id = ?;`
