1fb497541dfc6de6d185b5460ed3f7e548c41ac6
serhii <ewentualnie.sz@gmail.com>
2023-04-30 12:18:40 +0300

    :lipstick: add loading front

--- src/routers/books-router.ts
+++ src/routers/books-router.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import {getAll, getBook} from '../controllers/mysql-controller'
+import {getFront} from "../controllers/book-controller";
 
 const booksRouter = express.Router();
 
 booksRouter.get('/', getAll)
 booksRouter.get('/book/:id', getBook)
+booksRouter.get('/front', getFront)
 export default booksRouter;
