66a05cc92375ef799f3ee78846251bed4ae37bc5
serhii <ewentualnie.sz@gmail.com>
2023-05-08 21:28:02 +0300

    :sparkles: change saveBook method and add some another methods

--- src/services/mysql-service.ts
+++ src/services/mysql-service.ts
@@ -15,15 +15,26 @@ export async function findById(id: number): Promise<Book> {
 }
 
 export async function saveBook(book: Book): Promise<number | undefined> {
-    const storedBook = (await database.query(query.getBookByName, [book.name]))[0]
-    return storedBook.lenght ?
+    const storedBook = (await database.query(query.getBookByName, [book.name]))[0][0]
+    return storedBook ?
         undefined :
-        await database.query(
-            query.addBook,
-            [book.name, book.year, book.author, book.description, book.preview, book.title]
-        );
+        (await database.query(
+                query.addBook,
+                [book.name, book.year, book.description, book.preview, book.title, book.pages])
+        )[0].insertId;
 }
 
-export async function deleteById(id: number): Promise<number> {
-    return (await database.query(query.deleteBook, [id]))[0].changedRows;
+export async function deleteById(bookId: number): Promise<number> {
+    return (await database.query(query.deleteBook, [bookId]))[0].changedRows;
+}
+
+export async function saveAuthor(author: string): Promise<number> {
+    const storedAuthor = (await database.query(query.getAuthorByName, [author]))[0][0]
+    return storedAuthor ?
+        storedAuthor.id :
+        (await database.query(query.saveAuthor, [author]))[0].insertId
+}
+
+export async function savePair(bookId: number, authorId: number): Promise<void> {
+    await database.query(query.savePair, [bookId, authorId])
 }
