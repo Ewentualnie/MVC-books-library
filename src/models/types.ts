ba475dae3a8ea6daeec5f4d5d0f05b2076a9e74a
serhii <ewentualnie.sz@gmail.com>
2023-05-08 21:22:14 +0300

    :sparkles: remove unused interface and field from book type

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,18 +1,7 @@
-export interface FormDataBook extends FormData {
-    name: HTMLInputElement,
-    year: HTMLInputElement,
-    author1: HTMLInputElement,
-    author2: HTMLInputElement,
-    author3: HTMLInputElement,
-    preview: HTMLInputElement,
-    description: HTMLInputElement,
-}
-
 export type Book = {
     year: number;
     name: string;
     description: string;
-    author: string;
     preview: string;
     title: string;
     pages: string;
