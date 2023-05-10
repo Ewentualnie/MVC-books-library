e01566c8f9fc95ddca422588cd6a7ce83bbcb65e
serhii <ewentualnie.sz@gmail.com>
2023-05-10 09:41:13 +0300

    :sparkles: fix

--- src/services/mysql-service.ts
+++ src/services/mysql-service.ts
@@ -1,6 +1,6 @@
 import database from "../utils/db-connection";
 import query from "../utils/db-queries";
-import {Book} from "../models/types";
+import {newBook, Book} from "../models/types";
 
 export async function findLength(): Promise<number> {
     return (await database.query(query.getLength))[0][0]['COUNT(*)'];
@@ -14,7 +14,7 @@ export async function findById(id: number): Promise<Book> {
     return (await database.query(query.getBookById, [id]))[0][0];
 }
 
-export async function saveBook(book: Book): Promise<number | undefined> {
+export async function saveBook(book: newBook): Promise<number | undefined> {
     const storedBook = (await database.query(query.getBookByName, [book.name]))[0][0]
     return storedBook ?
         undefined :
