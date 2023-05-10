b222af4f7529f5eacca5bdac0f6bec21e1e554d3
serhii <ewentualnie.sz@gmail.com>
2023-05-10 14:58:48 +0300

    :seedling: add express.json

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ const port: number = 3000;
 app.set('view engine', 'ejs');
 app.set('views', source);
 
+app.use(express.json())
 app.use(express.static(source));
 app.use(morgan('common'));
 app.use(booksRouter);
