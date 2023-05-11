44c97a8fb14daf0fbc530cff5249a522eebf5f73
serhii <ewentualnie.sz@gmail.com>
2023-05-11 23:38:12 +0300

    add basic auth check

--- src/routers/admin-router.ts
+++ src/routers/admin-router.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import {addBook, deleteBook, getAll, getFront} from "../controllers/admin-controller";
 import fileUpload from "express-fileupload";
+import authCheck from "../controllers/auth-controller";
+
 
 
 const adminRouter = express.Router();
 
-adminRouter.get('/', getAll)
-adminRouter.post('/api/v1',fileUpload({createParentPath: true}), addBook)
-adminRouter.delete('/api/v1/:id', deleteBook)
+adminRouter.get('/',authCheck, getAll)
+adminRouter.post('/api/v1',authCheck,fileUpload({createParentPath: true}), addBook)
+adminRouter.delete('/api/v1/:id',authCheck, deleteBook)
 adminRouter.get('/front', getFront)
 
 export default adminRouter;
