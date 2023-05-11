f16db4a8748ba8161fe79823108188e628e4e3b5
serhii <ewentualnie.sz@gmail.com>
2023-05-11 20:28:33 +0300

    :green_heart: implement creatDBBackup method and refactor deleteMarkedBooks method

--- src/services/cron-service.ts
+++ src/services/cron-service.ts
@@ -1,12 +1,27 @@
 import {deletePair, getIDMarkedBooks, hardDeleteBooks} from "./mysql-service";
 import {MarkedBook} from "../models/types";
+import connection from "../models/connection";
+import mysqldump from 'mysqldump';
+import fs from "fs";
 
-export async function createDBBackup() {
-
+export function createDBBackup(): void {
+    const backupPath: string = process.env.backupPath || './dump/backups/';
+    const backupFileName: string = process.env.backupFileName || 'dump.sql';
+    const date: string = new Date().toISOString().substring(0, 10) + '_';
+    const fullPath: string = backupPath + date + backupFileName;
+    if (!fs.existsSync(backupPath)) {
+        fs.mkdirSync(backupPath);
+    }
+    fs.open(fullPath, 'w', (err) => {
+        if (!err) {
+            mysqldump({connection, dumpToFile: fullPath,})
+                .then(() => console.log('The database backup is stored in ' + fullPath));
+        }
+    })
 }
 
-export async function deleteMarkedBooks(): Promise<void> {
-    const markedAsDeleted: MarkedBook[] = await getIDMarkedBooks();
-    await Promise.all(markedAsDeleted.map((book: MarkedBook) => deletePair(book.id)))
-        .then(async (): Promise<void> => await hardDeleteBooks());
+export function deleteMarkedBooks(): void {
+    getIDMarkedBooks()
+        .then((markedBooks: MarkedBook[]) => Promise.all(markedBooks.map((book: MarkedBook) => deletePair(book.id)))
+            .then(async (): Promise<void> => await hardDeleteBooks()));
 }
