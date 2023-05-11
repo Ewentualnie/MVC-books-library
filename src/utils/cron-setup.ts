e62f6a1367418149db9722fbae8eb6cdc34e88e4
serhii <ewentualnie.sz@gmail.com>
2023-05-11 20:25:15 +0300

    :arrow_up: create cron schedule functions

--- src/utils/cron-setup.ts
+++ src/utils/cron-setup.ts
@@ -1,5 +1,10 @@
 import cron from 'node-cron';
 import {createDBBackup, deleteMarkedBooks} from "../services/cron-service";
+import {Schedule} from "../models/types";
 
-cron.schedule("0 0 * * *", createDBBackup)
-cron.schedule('0 2 * * *', deleteMarkedBooks)
+const runCron: Schedule = {
+    scheduleBackup: () => cron.schedule("0 0 * * *", createDBBackup),
+    scheduleDelete: () => cron.schedule('0 2 * * *', deleteMarkedBooks)
+}
+
+export default runCron;
